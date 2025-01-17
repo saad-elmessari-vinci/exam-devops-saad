b90bb616ec5708fbe412476962134f1ef219e88d
elmessari <saad.elmessari@student.vinci.be>
2025-01-17 17:26:58 +0100

    Refactor: usable constant main for all tests

--- spec/passwordChecker.test.js
+++ spec/passwordChecker.test.js
@@ -1,26 +1,21 @@
 import { Main } from "../src/passwordChecker.js";
 
 describe("passwordChecker function should return the answer ", function () {
-
-  it("Should return false for 1 characters ", function () {
     const main = new Main();
 
+  it("Should return false for 1 characters ", function () {
     const result = main.passwordChecker("a");
 
     expect(result).toBe(false);
   });
 
   it("Should return false for 4 characters ", function () {
-    const main = new Main();
-
     const result = main.passwordChecker("abcd");
 
     expect(result).toBe(false);
   });
 
   it("Should return false for 5 characters ", function () {
-    const main = new Main();
-
     const result = main.passwordChecker("abcde");
 
     expect(result).toBe(false);
