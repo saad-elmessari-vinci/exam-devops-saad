e33423e25176a43bd296dfde268ffd3e6fe350eb
elmessari <saad.elmessari@student.vinci.be>
2025-01-17 17:23:06 +0100

    add:when 4 character return false

--- spec/passwordChecker.test.js
+++ spec/passwordChecker.test.js
@@ -2,10 +2,18 @@ import { Main } from "../src/passwordChecker.js";
 
 describe("passwordChecker function should return the answer ", function () {
 
-  it("Should return false for 1 characters minute", function () {
+  it("Should return false for 1 characters ", function () {
     const main = new Main();
 
     const result = main.passwordChecker("a");
+
+    expect(result).toBe(false);
+  });
+
+  it("Should return false for 4 characters ", function () {
+    const main = new Main();
+
+    const result = main.passwordChecker("abcd");
 
     expect(result).toBe(false);
   });
--- src/passwordChecker.js
+++ src/passwordChecker.js
@@ -1,7 +1,7 @@
 export class Main {
     passwordChecker(password){
-        if (password.length === 1 ) return false
-        
+        if (password.length === 1 || password.length === 4) return false
+
         return true;
     }
 }
